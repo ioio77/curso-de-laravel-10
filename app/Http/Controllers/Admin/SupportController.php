52ce674d7d1db3d9928bd3c1d60bea13053874ea
sandro <sandro.novako@gmail.com>
2023-07-05 08:50:52 -0300

    suporte 02

--- app/Http/Controllers/Admin/SupportController.php
+++ app/Http/Controllers/Admin/SupportController.php
@@ -45,4 +45,40 @@ class SupportController extends Controller
 
     }
     
+    public function edit(Support $support, string|int $id){
+
+        if(!$support = $support->where('id',$id)->first()){
+            return back();
+       }
+       
+       return view('admin/supports.edit',compact('support'));
+
+    }
+
+    public function update(Request $request, Support $support, string $id){
+
+        if(!$support = $support->find($id)){
+            return back();
+       }
+
+       $support->update($request->only([
+            'subject','body'
+       ]));
+
+       return redirect()->route('supports.index');
+
+    }
+
+    public function destroy(string|int $id){
+
+        if(!$support = Support::find($id)){
+            return back();
+       }
+
+       $support->delete();
+
+       return redirect()->route('supports.index');
+
+    }
+        
 }
