3936f48d82abaac43e4fd0ad972fed9bd1b6cdbe
sandro <sandro.novako@gmail.com>
2023-07-05 07:24:53 -0300

    support01

--- app/Http/Controllers/Admin/SupportController.php
+++ app/Http/Controllers/Admin/SupportController.php
@@ -3,13 +3,46 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Support;
 use Illuminate\Http\Request;
 
 class SupportController extends Controller
 {
-    public function index(){
-        return view('admin/supports/index');        
-    }
+    public function index(Support $support){
+
+        $supports = $support->all();
         
+        return view('admin/supports/index',compact('supports'));        
+    }
+
+    public function show(string|int $id){
+       
+        // Support::find($id)
+        // Support::where('id',$id)->first();
+        // Support::where('id','!=',$id)->first();
+
+       if(!$support = Support::find($id)){
+            return back();
+       }
+       return view('admin/supports/show',compact('support'));
+    }
+
+    public function create(){
+        return view('admin/supports/create');
+
+    }
+     
+    public function store(Request $request, Support $support) {
+        
+       $data = $request->all();
+       $data['status'] = 'a';
+
+       /* Support::create($data);*/
+       $support = $support->create($data);
+       //dd($support);
+       return redirect()->route('supports.index');
+        
+
+    }
     
 }
--- resources/views/admin/supports/index.blade.php
+++ resources/views/admin/supports/index.blade.php
@@ -1,1 +1,27 @@
 <h1>Listagem dos suportes</h1>
+
+<a href="{{ route('supports.create') }}">Criar Dúvida</a>
+
+<table>
+    <thead>
+        <th>assunto</th>
+        <th>status</th>
+        <th>descrição</th>
+        <th></th>
+    </thead>
+</table>
+<tbody>
+    @foreach ($supports as $support )
+        <tr>
+            <td>{{ $support->subject }}</td>
+            <td>{{ $support->status }}</td>
+            <td>{{ $support->body }}</td>
+            <td>
+                <a href="{{ route('supports.show',$support->id)}}" >ir</a>
+            </td>
+
+        </tr>
+    </br>
+           
+    @endforeach
+</tbody>
